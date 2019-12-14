05b0bc1bdf4d08e1ccb254a2ec1d7f0b7f1adbe2
Aisha Rashed <aisha1rashed@gmail.com>
2019-12-14 05:07:06 +0000

    Automatically backed up by Learn

--- src/ChromeBoisDomain.js
+++ src/ChromeBoisDomain.js
@@ -29,11 +29,11 @@ export default class ChromeBoisDomain extends Component {
   /* if the key pressed was 'a', then it should call `resize` with '+'
   /* if the key pressed was 's', then it should call `resize` with '-' 
    */
-   handleKeyDown = (key)=>{
-     if(key == 's'){
+   handleKeyDown = (e)=>{
+     if(e.key == 's'){
         resize("-");
      }
-     else if(key == 'a'){
+     else if(e.key == 'a'){
         resize("+");
      }
    }
